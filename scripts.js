695978945f3b3b2aa193c8ba925c820167543b5d
Skyhawk Everheart <ean.baker@me.com>
2023-05-09 13:05:12 -0400

    Added CSS for header and added links in header. Also wrapped the click functions for buttons in a ready event.

--- scripts.js
+++ scripts.js
@@ -1,9 +1,12 @@
-$("#submitButton").click(function () {
-    document.querySelector("#userInputView").style.display = "none";
-    document.querySelector("#userOutputView").style.display = "block";
-});
+$(document).ready(function () {
 
-$("#resetButton").click(function () {
-    document.querySelector("#userOutputView").style.display = "none";
-    document.querySelector("#userInputView").style.display = "block";
+    $("#submitButton").click(function () {
+        document.querySelector("#userInputView").style.display = "none";
+        document.querySelector("#userOutputView").style.display = "block";
+    });
+
+    $("#resetButton").click(function () {
+        document.querySelector("#userOutputView").style.display = "none";
+        document.querySelector("#userInputView").style.display = "block";
+    });
 });
