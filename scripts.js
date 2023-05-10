8a98f13d4365b2d499f4354e4affd6a0af3a3b1a
Skyhawk Everheart <ean.baker@me.com>
2023-05-10 00:30:19 -0400

    Added HTML content and CSS styling.

--- scripts.js
+++ scripts.js
@@ -2,11 +2,13 @@ $(document).ready(function () {
     //This function hides the input box and displays the div for the output
     $("#submitButton").click(function () {
         document.querySelector("#userInputView").style.display = "none";
+        document.querySelector("#userInstructions").style.display = "none";
         document.querySelector("#userOutputView").style.display = "block";
     });
     //This function hides the output and displays the input box again
     $("#resetButton").click(function () {
         document.querySelector("#userOutputView").style.display = "none";
+        document.querySelector("#userInstructions").style.display = "block";
         document.querySelector("#userInputView").style.display = "block";
     });
     //This function stops submit button from refreshing page
