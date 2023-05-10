338525afd2d15228892264fd4ccf4c7789ff6d5c
Skyhawk Everheart <ean.baker@me.com>
2023-05-09 23:42:59 -0400

    Added JS to prevent submit button refreshing the page.

--- scripts.js
+++ scripts.js
@@ -1,12 +1,16 @@
 $(document).ready(function () {
-
+    //This function hides the input box and displays the div for the output
     $("#submitButton").click(function () {
         document.querySelector("#userInputView").style.display = "none";
         document.querySelector("#userOutputView").style.display = "block";
     });
-
+    //This function hides the output and displays the input box again
     $("#resetButton").click(function () {
         document.querySelector("#userOutputView").style.display = "none";
         document.querySelector("#userInputView").style.display = "block";
+    });
+    //This function stops submit button from refreshing page
+    $(document).on('submit', '#inputData', function() {
+        return false;
     });
 });
