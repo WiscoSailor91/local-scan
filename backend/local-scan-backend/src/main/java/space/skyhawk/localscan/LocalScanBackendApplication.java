6f40de31a3c4d6bb7891d9e77d1479636c8fd892
Cale Winebrenner <cale@appl.red>
2023-05-13 19:49:49 -0500

    Setting up Spring Boot 3.

---
+++ backend/local-scan-backend/src/main/java/space/skyhawk/localscan/LocalScanBackendApplication.java
@@ -0,0 +1,13 @@
+package space.skyhawk.localscan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalScanBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocalScanBackendApplication.class, args);
+    }
+
+}
---
+++ backend/local-scan-backend/src/test/java/space/skyhawk/localscan/LocalScanBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package space.skyhawk.localscan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalScanBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
