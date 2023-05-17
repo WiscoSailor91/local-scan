39279ef470b7d361c5c9ed2f105eb2509a0dfae7
Skyhawk Everheart <ean.baker@me.com>
2023-05-17 19:19:21 -0400

    Added javascript for click function to hide buttons for changing between light/dark mode. Added CSS for placing of dark/light mode buttons. More needed still.

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -20,4 +20,14 @@ $(document).ready(function () {
     $(document).on('submit', '#inputData', function() {
         return false;
     });
+    //This function hides light mode button, and changes CSS settings to light mode
+    $("#switchToLightButton").click(function () {
+        document.querySelector("#switchToDarkButton").style.display = "block";
+        document.querySelector("#switchToLightButton").style.display = "none";
+    });
+    //This function hides dark mode button, and changes CSS settings to dark mode
+    $("#switchToDarkButton").click(function () {
+        document.querySelector("#switchToDarkButton").style.display = "none";
+        document.querySelector("#switchToLightButton").style.display = "block";
+    });
 });
